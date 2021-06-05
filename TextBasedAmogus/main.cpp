11ca21977cfba11ebeca7b4daa21a94c75962654
Siohfox <72400408+Siohfox@users.noreply.github.com>
2021-06-05 16:54:08 +0100

    Commented main

--- TextBasedAmogus/main.cpp
+++ TextBasedAmogus/main.cpp
@@ -4,14 +4,21 @@
 
 int main(int argc, char* argv[])
 {
+	// Create local renderer
 	SDL_Renderer* renderer;
 
+	// Create object instance of SDL manager, passing in screen size
 	SDLManager SDL(800, 800);
 
+	// Return SDLInit's renderer to the local one here
+	// SDL init will also set up all of the other SDL related components
 	renderer = SDL.SDLInit();
 
+	// Create instance of the menu, passing in the initialized renderer
+	// The renderer will be passed down along the way
 	Menu menu(renderer);
 
+	// Start the menu loop
 	menu.MenuLoop();
 
 	return 0;
